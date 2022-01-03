2735f9cb64b65da5e94df8428ab060b6a5e882c6
ProcyonDev <96466601+ProcyonDev@users.noreply.github.com>
2022-01-03 10:27:29 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "pProtect"
+description "pProtect"
+author "Procyon"
+version "1.00"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
